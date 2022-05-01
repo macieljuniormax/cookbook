19eb2a5c9b1b65ae2dae967139eb587aff507d83
Maciel Júnior <macielfcjunior@icloud.com>
2022-05-01 11:52:36 -0300

    Começando com BD

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,5 @@
 class HomeController < ApplicationController
   def index
-    r1 = Recipe.new(name: 'Pudim', recipe_type:'Sobremesa')
-    r2 = Recipe.new(name: 'Bolo de Cenoura', recipe_type:'Sobremesa')
-
-    @recipes = [r1, r2]
+    @recipes = Recipe.all
   end
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,9 +1,2 @@
-class Recipe
-  attr_reader :name, :recipe_type 
-  # atributos somente leitura
-  def initialize(name:, recipe_type:)
-    @name = name
-    @recipe_type = recipe_type
-  end
+class Recipe < ApplicationRecord
 end
-
